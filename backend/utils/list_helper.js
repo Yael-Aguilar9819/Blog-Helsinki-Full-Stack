3445e257da10446a5b416b55e7bf2aa7ff2344e0
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-07-23 01:24:14 -0500

    Used jest to create a series of tests

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = blogs =>
 // ...
   1;
 
+
+const totalLikes = arrayOfBlogs => {
+  const valueSum = (accum, curBlog) => accum + curBlog.likes;
+  return arrayOfBlogs.reduce(valueSum, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes
 };
