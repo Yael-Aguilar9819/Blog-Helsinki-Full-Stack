4fe3b52349ba0432c9adf75ebf61a9f7de2af47a
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-07-23 21:56:35 -0500

    Created function favoriteBlog and starting to use the test

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,13 +2,19 @@ const dummy = blogs =>
 // ...
   1;
 
-
 const totalLikes = arrayOfBlogs => {
+  //Using an array of blogs with reduce now its totally functional
   const valueSum = (accum, curBlog) => accum + curBlog.likes;
-  return arrayOfBlogs.reduce(valueSum, 0)
+  return arrayOfBlogs.reduce(valueSum, 0) // The starting value is 0 so it works corcectly
+}
+
+const favoriteBlog = arrayOfBlogs => {
+  const mostNumberOfLikes = (biggestSeen, currBlog) => biggestSeen.likes > currBlog.likes ? null : biggestSeen = currBlog;
+  return arrayOfBlogs.reduce(mostNumberOfLikes) // The starting value is 0 so it works corcectly
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
