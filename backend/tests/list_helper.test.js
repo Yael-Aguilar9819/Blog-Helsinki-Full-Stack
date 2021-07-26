f38967d0772deb7668d9400854bb120f10e92442
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-07-26 17:49:59 -0500

    Refactored favorite blog and corrected the style in ESLint

--- backend/tests/list_helper.test.js
+++ backend/tests/list_helper.test.js
@@ -9,38 +9,36 @@ test('dummy returns one', () => {
   expect(result).toBe(1);
 });
 
-
 // This is a series of test with common possible cases, may be expanded in the future
 describe('total likes', () => {
-
   test('empty list returns 0', () => {
-    const result = listHelper.totalLikes([])
-    expect(result).toBe(0)
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
   });
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(mockBlogs.listWithOneBlog)
-    expect(result).toBe(5)
-  })
+    const result = listHelper.totalLikes(mockBlogs.listWithOneBlog);
+    expect(result).toBe(5);
+  });
 
   test('A big list is correctly calculated', () => {
-    const result = listHelper.totalLikes(mockBlogs.listOfBlogs)
-    expect(result).toBe(36)
-  })
-})
+    const result = listHelper.totalLikes(mockBlogs.listOfBlogs);
+    expect(result).toBe(36);
+  });
+});
 
 describe('Max number of likes', () => {
-  test("empty list returns 0", () => {
-    const result = listHelper.favoriteBlog([])
-  })
+  test('empty list returns 0', () => {
+    const result = listHelper.favoriteBlog([]);
+  });
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.favoriteBlog(mockBlogs.listWithOneBlog)
-    expect(result).toBe(mockBlogs.listWithOneBlog[0])
-  })
+    const result = listHelper.favoriteBlog(mockBlogs.listWithOneBlog);
+    expect(result).toBe(mockBlogs.listWithOneBlog[0]);
+  });
 
   test('A big list is correctly calculated with the blog with the max number of likes returned', () => {
-    const result = listHelper.favoriteBlog(mockBlogs.listOfBlogs)
-    expect(result).toBe(mockBlogs.listOfBlogs[2])
-  })
-})
+    const result = listHelper.favoriteBlog(mockBlogs.listOfBlogs);
+    expect(result).toBe(mockBlogs.listOfBlogs[2]);
+  });
+});
--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,25 +1,22 @@
-const dummy = blogs =>
-// ...
-  1;
+const dummy = blogs => 1;
 
 const totalLikes = arrayOfBlogs => {
-  //Using an array of blogs with reduce now its totally functional
+  // Using an array of blogs with reduce now its totally functional
   const valueSum = (accum, curBlog) => accum + curBlog.likes;
-  return arrayOfBlogs.reduce(valueSum, 0) // The starting value is 0 so it works corcectly
-}
+  return arrayOfBlogs.reduce(valueSum, 0); // The starting value is 0 so it works corcectly
+};
 
-const favoriteBlog = arrayOfBlogs => { //Using reduce it's now not very efficient
-  //But its immutable
+const favoriteBlog = arrayOfBlogs => { // Using reduce it's not very efficient but its immutable
+  // Now it's a single line, and much clearer
   const mostNumberOfLikes = (biggestSeen, currBlog) => {
-    //it's a simple if else, to be changed into a ternary single line
-    if (biggestSeen.likes < currBlog.likes) {return currBlog}
-    return biggestSeen;
+    (biggestSeen.likes < currBlog.likes ? currBlog : biggestSeen);
   };
-  return arrayOfBlogs.reduce(mostNumberOfLikes, {likes :-1}) // The starting value is 0 so it works corcectly
-} 
+  // The starting value is -1 so it can compare correctly
+  return arrayOfBlogs.reduce(mostNumberOfLikes, { likes: -1 });
+};
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
 };
