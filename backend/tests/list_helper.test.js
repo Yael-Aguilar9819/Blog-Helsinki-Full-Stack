8b7c1efdaddd1436dd5ac54cc98bfae0a6902f97
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-07-27 21:56:49 -0500

    Tried to use Lodash groupby

--- backend/tests/list_helper.test.js
+++ backend/tests/list_helper.test.js
@@ -28,8 +28,9 @@ describe('total likes', () => {
 });
 
 describe('Max number of likes', () => {
-  test('empty list returns 0', () => {
+  test('empty list returns empty object', () => {
     const result = listHelper.favoriteBlog([]);
+    expect(result).toEqual({likes: -1});
   });
 
   test('when list has only one blog, equals the likes of that', () => {
@@ -40,5 +41,24 @@ describe('Max number of likes', () => {
   test('A big list is correctly calculated with the blog with the max number of likes returned', () => {
     const result = listHelper.favoriteBlog(mockBlogs.listOfBlogs);
     expect(result).toBe(mockBlogs.listOfBlogs[2]);
+  });
+});
+
+
+
+describe("Authors with the most number of blogs", () => {
+  test('empty list returns 0', () => {
+    const result = listHelper.mostBlogs([]);
+    expect(result).toEqual({})
+  });
+
+  test('when list has only one blog, equals the author of that', () => {
+    const result = listHelper.mostBlogs(mockBlogs.listWithOneBlog);
+    expect(result).toEqual({author: "Edsger W. Dijkstra", blogs: 1});
+  });
+
+  test('A big list is correctly calculated with the blog with the max number of likes returned', () => {
+    const result = listHelper.favoriteBlog(mockBlogs.listOfBlogs);
+    expect(result).toEqual({author: "Robert C. Martin", blogs: 3});
   });
 });
--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -1,5 +1,7 @@
 const dummy = blogs => 1;
 
+const _ = require('lodash');
+
 const totalLikes = arrayOfBlogs => {
   // Using an array of blogs with reduce now its totally functional
   const valueSum = (accum, curBlog) => accum + curBlog.likes;
@@ -9,14 +11,21 @@ const totalLikes = arrayOfBlogs => {
 const favoriteBlog = arrayOfBlogs => { // Using reduce it's not very efficient but its immutable
   // Now it's a single line, and much clearer
   const mostNumberOfLikes = (biggestSeen, currBlog) => {
-    (biggestSeen.likes < currBlog.likes ? currBlog : biggestSeen);
+    return (biggestSeen.likes < currBlog.likes ? currBlog : biggestSeen);
   };
   // The starting value is -1 so it can compare correctly
   return arrayOfBlogs.reduce(mostNumberOfLikes, { likes: -1 });
 };
 
+const mostBlogs = arrayOfBlogs => {
+  const objectAnswer = _.groupBy(arrayOfBlogs, "author");
+  console.log(objectAnswer)
+  return objectAnswer;
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
