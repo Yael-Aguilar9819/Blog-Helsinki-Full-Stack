0777f80f4b9877effe690635615d883555317a0e
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-10-18 19:12:04 -0500

    Now there is a form to handle each login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +13,38 @@ const App = () => {
     )  
   }, [])
 
+  
+  
+// login form handles the function of the 
+const loginForm = () => (
+  <form onSubmit={"welp"}>
+    {/* each of this 2 are an input. To be reworked in a component */}
+    <div>
+      username
+        <input
+        type="text"
+        value={username}
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)}
+      />
+    </div>
+    <div>
+      password
+        <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+    </div>
+    <button type="submit">login</button>
+  </form>      
+)
+
   return (
     <div>
+      {/* This invokes the loginForm  */}
+      {loginForm()}
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
