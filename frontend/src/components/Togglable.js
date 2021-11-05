c93378df7a497ed06f7b0ef47c54cf5d0242d1de
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-11-05 03:23:50 -0600

    Created Togglable component that will hide or show each component with a button

---
+++ frontend/src/components/Togglable.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Togglable = ({buttonLabel}) => {
+  const [visible, setVisible] = useState(false)
+
+  // This is the that will be shown depending of the visibility
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  // Each time the button is pressed, the visible property
+  // Will change from false -> true or true -> false
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default Togglable
