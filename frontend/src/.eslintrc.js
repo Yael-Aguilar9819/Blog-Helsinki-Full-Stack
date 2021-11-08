e9f3b1d614501d9be3e867aa2b622481f7269d2e
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-11-07 22:23:07 -0600

    Created .eslintrc.js to be used in the frontend

---
+++ frontend/src/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+      'no-underscore-dangle': 'off',
+      'no-use-before-define': [
+        'error', { functions: false, classes: false, variables: false },
+      ],
+      'arrow-parens': [
+        'error', 'as-needed',
+      ],
+      'linebreak-style': [
+        'error',
+        'windows',
+      ],
+    },
+  };
+      
