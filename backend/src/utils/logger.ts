a21f1ac748345dab9c85b5d191b70ce14dce6e0d
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-12-12 03:36:21 -0600

    Moved the folder request to go inside of test

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
-const info = (...params) => {
+const info = (...params: any[]) => {
   if (process.env.NODE_ENV !== 'test') {
     console.log(...params);
   }
 };
 
-const error = (...params) => {
+const error = (...params: any[]) => {
   if (process.env.NODE_ENV !== 'test') {
     console.log(...params);
   }
