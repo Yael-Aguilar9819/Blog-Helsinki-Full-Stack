6f4b3d17c20dc131847e02db49959619c0d3d4b7
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-12-13 22:26:46 -0600

    modified a big part of the index and app segments, now they search for a dist folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const express = require('express');
 const app = require('./app'); // The actual Express application
-const config = require('./utils/config');
-const logger = require('./utils/logger');
+const config = require('./dist/utils/config');
+const logger = require('./dist/utils/logger');
 
 // This creates a server for the app
 const server = http.createServer(app);
